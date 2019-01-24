321b51d337c9c332856414b7f717cfa4e0a3f218
mgrinko <glukym@gmail.com>
2019-01-24 19:16:25 +0200

    Components were added

---
+++ js/phones/components/filter.js
@@ -0,0 +1,24 @@
+export default class Filter {
+  constructor({ element }) {
+    this._element = element;
+
+    this._render();
+  }
+
+  _render() {
+    this._element.innerHTML = `
+      <p>
+        Search:
+        <input>
+      </p>
+
+      <p>
+        Sort by:
+        <select>
+          <option value="name">Alphabetical</option>
+          <option value="age">Newest</option>
+        </select>
+      </p>
+    `;
+  }
+}
