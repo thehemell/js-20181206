6efbe2c5d821898834bc7cdbb9b23761c62ff49c
Данил <danil@pandaworks.ru>
2019-01-27 16:51:01 +0500

    Added function for button back, gallery, add cart.

--- js/phones/components/phone-catalog.js
+++ js/phones/components/phone-catalog.js
@@ -1,23 +1,27 @@
 'use strict';
 
 export default class PhoneCatalog {
-  constructor({ element, phones, onPhoneSelected }) {
+  constructor({ element, phones, onPhoneSelected, onPhoneAddCart }) {
     this._element = element;
     this._phones = phones;
     this._onPhoneSelected = onPhoneSelected;
+    this._onPhoneAddCart = onPhoneAddCart;
 
     this._render();
 
     this._element.addEventListener('click', (event) => {
-      const phoneLink = event.target.closest('[data-element="phone-link"]');
+      const phoneLink = event.target.closest('[data-element]');
+      const phoneElement = phoneLink.closest('[data-element="phone"]');
+      const elType = phoneLink.dataset.element;
 
-      if (!phoneLink) {
+      if (elType === 'phone-link') {
+        this._onPhoneSelected(phoneElement.dataset.phoneId);
+      } else if (elType === 'add-cart') {
+        this._onPhoneAddCart(phoneElement.dataset.phoneName);
+      } else {
         return;
       }
 
-      const phoneElement = phoneLink.closest('[data-element="phone"]');
-
-      this._onPhoneSelected(phoneElement.dataset.phoneId);
     });
   }
 
@@ -38,6 +42,7 @@ export default class PhoneCatalog {
           <li
             data-element="phone"
             data-phone-id="${ phone.id }"
+            data-phone-name="${ phone.name }"
             class="thumbnail"
           >
             <a
@@ -49,7 +54,7 @@ export default class PhoneCatalog {
             </a>
   
             <div class="phones__btn-buy-wrapper">
-              <a class="btn btn-success">
+              <a class="btn btn-success" data-element="add-cart">
                 Add
               </a>
             </div>
--- js/phones/components/phone-viewer.js
+++ js/phones/components/phone-viewer.js
@@ -1,7 +1,33 @@
 export default class PhoneViewer {
-  constructor({ element }) {
+  constructor({ element, onDetailBack, onDetailAddCart }) {
     this._element = element;
+    this._onDetailBack = onDetailBack;
+    this._onDetailAddCart = onDetailAddCart;
 
+    this._element.addEventListener('click', (event) => {
+      const elButton = event.target.closest('[data-element]');
+      const phoneElement = elButton.closest('[data-element="detail"]');
+      const elType = elButton.dataset.element;
+
+      if (elType === 'back-catalog') {
+        this._onDetailBack();
+      } else if (elType === 'add-cart') {
+        this._onDetailAddCart(phoneElement.dataset.phoneName);
+      } else {
+        return;
+      }
+
+    });
+
+    this._element.addEventListener('click', (event) => {
+      const image = event.target.closest('[data-image="img"]').src;
+
+      if (!image) {
+        return;
+      }
+
+      this._element.querySelector('.phone').setAttribute('src', image)
+    });
   }
 
   hide() {
@@ -15,40 +41,31 @@ export default class PhoneViewer {
 
     this._render();
   }
+
   _render() {
     let phone = this._phoneDetails;
 
     this._element.innerHTML = `
-      <img class="phone" src="${ phone.images[0] }">
-
-      <button>Back</button>
-      <button>Add to basket</button>
-  
-  
-      <h1>${ phone.name }</h1>
-  
-      <p>${ phone.description }</p>
-  
-      <ul class="phone-thumbs">
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.0.jpg">
-        </li>
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.1.jpg">
-        </li>
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.2.jpg">
-        </li>
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.3.jpg">
-        </li>
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.4.jpg">
-        </li>
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.5.jpg">
-        </li>
-      </ul>
+    <div data-element="detail"
+         data-phone-name="${ phone.name }">
+          <img class="phone" src="${ phone.images[0] }">
+    
+          <button data-element="back-catalog">Back</button>
+          <button data-element="add-cart">Add to basket</button>
+      
+      
+          <h1>${ phone.name }</h1>
+      
+          <p>${ phone.description }</p>
+      
+          <ul class="phone-thumbs">
+            ${phone.images.map(image => `
+            <li>
+              <img src="${ image }" data-image="img">
+            </li>
+            `).join('') }
+          </ul>
+    </div>
     `;
   }
 }
